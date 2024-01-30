0adfa16700923cffb0c44f5f06b6288c7bdd6c13
David B <d.jbradley.ii@gmail.com>
2024-01-29 20:35:32 -0800

    Adds Hello world function

--- netlify/functions/helloworld/index.mjs
+++ netlify/functions/helloworld/index.mjs
@@ -1,5 +1,7 @@
 export default async (req, context) => {
-  return new Response("Hello, World!");
+  const functionResponse = new String("Hello, World!");
+  const options = { status: 200, statusText: "Serverless Response" };
+  return new Response(functionResponse, options);
 };
 
 export const config = {
