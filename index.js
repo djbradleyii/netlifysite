5a7f4ae5535e324bd1d83e35f572bffe591e8730
David B <d.jbradley.ii@gmail.com>
2024-01-29 20:44:31 -0800

    Adds Hello world function

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const fetchBtn = document.getElementById("fetchFunction");
 fetchBtn.addEventListener("click", () => {
-  fetch("https://www.dreamcloudjourney.com/hello").then((resp) => {
-    const data = resp.text();
-    const responseTextArea = document.getElementById("btnResponse");
-    responseTextArea.textContent = data;
-  });
+  fetch("https://www.dreamcloudjourney.com/hello")
+    .then((resp) => resp.text())
+    .then((data) => {
+      const responseTextArea = document.getElementById("btnResponse");
+      responseTextArea.textContent = data;
+    });
 });
