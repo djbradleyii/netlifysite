9bb1a00ba8b5ad0aa5b6f5c1b0d2a168f1624c70
David B <d.jbradley.ii@gmail.com>
2024-01-29 19:56:14 -0800

    Adds Hello world function

--- netlify/functions/helloworld/index.mjs
+++ netlify/functions/helloworld/index.mjs
@@ -1,7 +1,9 @@
 export default async (req, context) => {
+  console.log("req: ", req);
+  console.log("context: ", context);
   return new Response(`Hello, World!`);
 };
 
 export const config = {
-  path: "/netlify/hello",
+  path: "/hello",
 };
